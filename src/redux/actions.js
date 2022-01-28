5a5a2f7f361d5b0ccd308ff80b06762e5255cf98
ukm25 <quang251199@gmail.com>
2022-01-28 13:51:32 +0700

    holetex redux

---
+++ src/redux/actions.js
@@ -0,0 +1,15 @@
+//action creators => function
+
+export const addTodo = (data) => {
+    return {
+        type: 'todoList/addTodo',
+        payload: data
+    }
+}
+
+export const searchFilterChange = (text) => {
+    return {
+        type: 'filters/searchFilterChange',
+        payload: text,
+    }
+}
---
+++ src/redux/reducer.js
@@ -0,0 +1,38 @@
+const initState = {
+    filters: {
+        search: '',
+        status: 'All',
+        priority: [],
+    },
+    todoList: [
+        { id: 1, name: 'Learn Yoga', completed: false, priority: 'Medium'},
+        { id: 2, name: 'Learn Redux', completed: true, priority: 'High'},
+        { id: 3, name: 'Learn Javascript', completed: false, priority: 'Low'},
+    ]
+}
+
+const rootReducer = (state = initState, action) => {
+    // console.log(state, action)
+    switch(action.type)  {
+        case 'todoList/addTodo':
+            return {
+                ...state,
+                todoList: [
+                    ...state.todoList,
+                    action.payload,
+                ]
+            }
+        case 'filters/searchFilterChange':
+            return {
+                ...state,
+                filters: {
+                    ...state.filters,
+                    search: action.payload,
+                }
+            }
+        default:
+            return state;
+    }
+}
+
+export default rootReducer
